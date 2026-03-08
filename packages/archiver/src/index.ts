42449d63056d9ebb9677ceb70f733fee3fe9d612
pranavvdesai <pranavdesai.psd@gmail.com>
2026-03-08 10:41:57 +0530

    feat: schema

--- packages/archiver/src/index.ts
+++ packages/archiver/src/index.ts
@@ -1,1 +1,19 @@
-export const archiverPlaceholder = 'archiver package placeholder';
+export {
+  FREEZE_ARCHIVE_SCHEMA_V1,
+  X_PLATFORM,
+  type ArchiveContentMode,
+  type ArchivedTweetDocumentV1,
+  type ArchivedTweetRecord,
+  type ArchiveSource,
+  type AuthorMetadata,
+  type CashtagEntity,
+  type FreezeArchiveSchemaVersion,
+  type HashtagEntity,
+  type MediaReference,
+  type MentionEntity,
+  type SourcePlatform,
+  type TextEntities,
+  type TweetMetadata,
+  type TweetReference,
+  type UrlEntity
+} from './archive-schema';
